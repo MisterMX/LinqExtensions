bf2c8c3ac268f8fd79cb83b806b64f596b6e3f97
MisterMX <mbxd12@web.de>
2016-08-26 20:13:34 +0200

    Add left outer join extension for IEnumerable

--- LinqExtensions/LinqExtensions/LinqExtensions.cs
+++ LinqExtensions/LinqExtensions/LinqExtensions.cs
@@ -13,8 +13,8 @@ namespace LinqExtensions
         #region Extension methods
 
         /// <summary>
-        /// Performs a left outer join between an <see cref="IQueryable{TOuter}"/> and an
-        /// <see cref="IEnumerable{TInner}"/>.
+        /// Performs a left outer join between an <see cref="IQueryable{TLeft}"/> and an
+        /// <see cref="IEnumerable{TRight}"/>.
         /// </summary>
         /// <typeparam name="TLeft">The type of the left collection.</typeparam>
         /// <typeparam name="TRight">The type of the right collection.</typeparam>
@@ -33,17 +33,63 @@ namespace LinqExtensions
             Expression<Func<TRight, TKey>> innerKeySelector,
             Expression<Func<JoinResult<TLeft, TRight>, TResult>> resultSelector)
         {
-            IQueryable<GroupJoinResult<TLeft, TRight>> groupJoinResult = outer.GroupJoin(
-                inner,
-                outerKeySelector,
-                innerKeySelector,
-                (o, i) => new GroupJoinResult<TLeft, TRight>
-                {
-                    Key = o,
-                    Values = i
-                });
+            IQueryable<GroupJoinResult<TLeft, TRight>> groupJoinResult = outer
+                .GroupJoin(
+                    inner,
+                    outerKeySelector,
+                    innerKeySelector,
+                    (o, i) => new GroupJoinResult<TLeft, TRight>
+                    {
+                        Key = o,
+                        Values = i
+                    });
 
             IQueryable<TResult> leftOuterJoinResults = groupJoinResult
+                .SelectMany(
+                    x => x.Values.Select(c => new JoinResult<TLeft, TRight>
+                    {
+                        Left = x.Key,
+                        Right = c
+                    }))
+                .Select(resultSelector);
+
+            return leftOuterJoinResults;
+        }
+
+
+        /// <summary>
+        /// Performs a left outer join between an <see cref="IEnumerable{TLeft}"/> and an
+        /// <see cref="IEnumerable{TRight}"/>.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left collection.</typeparam>
+        /// <typeparam name="TRight">The type of the right collection.</typeparam>
+        /// <typeparam name="TKey">The type of the key that is used for the join.</typeparam>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="outer">The left collection.</param>
+        /// <param name="inner">The right collection.</param>
+        /// <param name="outerKeySelector">The key selector expression for the left collection.</param>
+        /// <param name="innerKeySelector">The key selector expression for the right collection.</param>
+        /// <param name="resultSelector">The result selector expression.</param>
+        /// <returns>the resulting <see cref="IQueryable{TResult}"/>.</returns>
+        public static IEnumerable<TResult> LeftOuterJoin<TLeft, TRight, TKey, TResult>(
+            this IEnumerable<TLeft> outer,
+            IEnumerable<TRight> inner,
+            Func<TLeft, TKey> outerKeySelector,
+            Func<TRight, TKey> innerKeySelector,
+            Func<JoinResult<TLeft, TRight>, TResult> resultSelector)
+        {
+            IEnumerable<GroupJoinResult<TLeft, TRight>> groupJoinResult = outer
+                .GroupJoin(
+                    inner,
+                    outerKeySelector,
+                    innerKeySelector,
+                    (o, i) => new GroupJoinResult<TLeft, TRight>
+                    {
+                        Key = o,
+                        Values = i
+                    });
+
+            IEnumerable<TResult> leftOuterJoinResults = groupJoinResult
                 .SelectMany(
                     x => x.Values.Select(c => new JoinResult<TLeft, TRight>
                     {
