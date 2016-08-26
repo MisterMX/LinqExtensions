d043543569be5b37c37addeeb69cb5d6396a171f
MisterMX <mbxd12@web.de>
2016-08-26 21:08:18 +0200

    Add expression extension for 'or' and 'and' composition

---
+++ LinqExtensions/LinqExtensions/Expressions/ExpressionExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqExtensions.Expressions
+{
+    /// <summary>
+    /// Contains extension methods for LINQ expressions.
+    /// </summary>
+    public static class ExpressionExtensions
+    {
+        #region Extension methods
+
+        /// <summary>
+        /// Composes two <see cref="Expression{T}"/> using another <see cref="Expression"/>.
+        /// </summary>
+        /// <typeparam name="TDelegate">the delegate type.</typeparam>
+        /// <param name="first">The first <see cref="Expression{TDelegate}"/>.</param>
+        /// <param name="second">The second <see cref="Expression{TDelegate}"/>.</param>
+        /// <param name="merge">
+        /// The <see cref="Expression"/> that is used to combine the first and the second <see cref="Expression{TDelegate}"/>.
+        /// </param>
+        /// <returns>the composed <see cref="Expression{TDelegate}"/>.</returns>
+        public static Expression<TDelegate> Compose<TDelegate>(
+            this Expression<TDelegate> first,
+            Expression<TDelegate> second,
+            Func<Expression, Expression, Expression> merge)
+        {
+            // Build parameter map (from parameters of second to parameters of first)
+            Dictionary<ParameterExpression, ParameterExpression> map = first.Parameters
+                .Select((f, i) => new
+                {
+                    f, s = second.Parameters[i]
+                })
+                .ToDictionary(
+                    p => p.s,
+                    p => p.f);
+
+            // Replace parameters in the second lambda expression with parameters from the first
+            Expression secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+            // Apply composition of lambda expression bodies to parameters from the first expression 
+            return Expression.Lambda<TDelegate>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+
+        /// <summary>
+        /// Combines the following <see cref="Expression{TDelegate}"/>'s with an 'and' operator.
+        /// </summary>
+        /// <typeparam name="TDelegate"></typeparam>
+        /// <param name="first">The first <see cref="Expression{TDelegate}"/>.</param>
+        /// <param name="second">The second <see cref="Expression{TDelegate}"/>.</param>
+        /// <returns>the composed <see cref="Expression{TDelegate}"/>.</returns>
+        public static Expression<Func<TDelegate, bool>> And<TDelegate>(
+            this Expression<Func<TDelegate, bool>> first,
+            Expression<Func<TDelegate, bool>> second)
+        {
+            return first.Compose(second, Expression.And);
+        }
+
+
+        /// <summary>
+        /// Combines the following <see cref="Expression{TDelegate}"/>'s with an 'and' operator.
+        /// </summary>
+        /// <typeparam name="TDelegate"></typeparam>
+        /// <param name="first">The first <see cref="Expression{TDelegate}"/>.</param>
+        /// <param name="second">The second <see cref="Expression{TDelegate}"/>.</param>
+        /// <returns>the composed <see cref="Expression{TDelegate}"/>.</returns>
+        public static Expression<Func<TDelegate, bool>> Or<TDelegate>(
+            this Expression<Func<TDelegate, bool>> first,
+            Expression<Func<TDelegate, bool>> second)
+        {
+            return first.Compose(second, Expression.Or);
+        }
+
+        #endregion
+    }
+}
---
+++ LinqExtensions/LinqExtensions/Expressions/ParameterRebinder.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace LinqExtensions.Expressions
+{
+    /// <summary>
+    /// Provides the ability of rebinding the parameters of an <see cref="Expression"/>.
+    /// </summary>
+    internal class ParameterRebinder : ExpressionVisitor
+    {
+        #region Fields
+
+        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParameterRebinder"/> class.
+        /// </summary>
+        /// <param name="map">
+        /// The <see cref="ParameterExpression"/>s of the first <see cref="Expression"/> mapped to them of
+        /// the second.
+        /// </param>
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        #endregion
+
+        #region Public static methods
+
+        /// <summary>
+        /// Replace the parameters of the following expression with them of another.
+        /// </summary>
+        /// <param name="map">
+        /// The <see cref="ParameterExpression"/>s of the parameter <see cref="Expression"/> mapped to other paramters.
+        /// </param>
+        /// <param name="exp">The <see cref="Expression"/> whose parameters should be replaced.</param>
+        /// <returns>the <see cref="Expression"/> with the replaced parameters.<see cref=""/></returns>
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        #endregion
+
+        #region Overriden methods
+
+        /// <summary>
+        /// Visits the <see cref="ParameterExpression"/>.
+        /// </summary>
+        /// <param name="p">The expression to visit.</param>
+        /// <returns>
+        /// The modified expression, if it or any subexpression was modified;
+        /// otherwise, returns the original expression.
+        /// </returns>
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            ParameterExpression replacement;
+
+            if (map.TryGetValue(p, out replacement))
+            {
+                p = replacement;
+            }
+
+            return base.VisitParameter(p);
+        }
+
+        #endregion
+    }
+}
